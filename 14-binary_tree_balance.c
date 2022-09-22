59f553d81f06bb3781ae584cc580fe08ed0b6079
Aisosa <allysonaghedo@gmail.com>
2022-09-22 21:15:02 +0100

    balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -17,8 +17,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	return (1 + right);
 }
 /**
- * binary_tree_balance - function that measures the balance factor of a binary tree
+ * binary_tree_balance - function that measures the balance factor of
+ * a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
+ * Return: balance factor
  */
 
 int binary_tree_balance(const binary_tree_t *tree)
