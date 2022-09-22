e2374f14bffe25b334876b6e6ee9bcfb2ba122f3
Aisosa <allysonaghedo@gmail.com>
2022-09-22 12:29:30 +0100

    depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,12 +8,10 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth;
 
-	if (tree == NULL)
-		return (0);
-	if (tree->parent == NULL)
+	if (tree == NULL || tree->parent == NULL)
 		return (0);
 	else
-		depth = (binary_tree_depth(tree->parent));
+		depth = binary_tree_depth(tree->parent);
 
 	return depth + 1;
 }
