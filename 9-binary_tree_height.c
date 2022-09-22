049dafb8fd94d3ce9e463c89fddc32a52d9af439
Aisosa <allysonaghedo@gmail.com>
2022-09-22 10:50:09 +0100

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,7 +8,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_counter, right_counter;
 
-	if (tree == NULL || tree->left == NULL || tree->right == NULL)
+	if (tree == NULL || tree->left == NULL && tree->right == NULL)
 		return (0);
 	left_counter =  binary_tree_height(tree->left);
 	right_counter =  binary_tree_height(tree->right);
